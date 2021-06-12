c4633903dc569ae16e03a297135b712525893fcc
Arka2410 <arkagupta568@gmail.com>
2021-06-12 11:46:37 +0530

    First Commit

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeApp()
+	{
+		System.out.println("NativeApp");
+	}
+	@Test
+	public void HybridApp()
+	{
+		System.out.println("HybridApp");
+	}
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/SeleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void WebBrowserAutomation()
+	{
+		System.out.println("WebBrowserAutomation");
+	}
+	@Test
+	public void MobileBrowserAutomation()
+	{
+		System.out.println("MobileBrowserAutomation");
+	}
+	
+
+}
